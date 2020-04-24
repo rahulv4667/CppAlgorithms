74511ca974f9f354ece669a503f27761ac963636
rahulv4667 <4667rahul@gmail.com>
2020-04-24 19:46:39 +0530

    added non-linear sorting

--- C++Algorithms/main.cpp
+++ C++Algorithms/main.cpp
@@ -13,16 +13,17 @@ int main(void) {
 		std::cin >> temp;
 		v.push_back(temp);
 	}
+
 	/*v.push_back(5);
 	v.push_back(4);
 	v.push_back(3);
 	v.push_back(1);
 	v.push_back(2);*/
 	//insertionSort(v);
-	mergeSort(v);
+	//mergeSort(v);
+	selectionSort(v);
 	std::cout << std::endl << "Elements after sorting:";
 	for (int i = 0; i < n; i++)
 		std::cout << v[i] << "\t";
-	char c;
-	std::cin >> c;
+	
 }
--- C++Algorithms/sort.h
+++ C++Algorithms/sort.h
@@ -2,6 +2,32 @@
 #include<iostream>
 #include<vector>
 
+
+template<typename T>
+void print(std::vector<T>& vec) {
+	std::cout << std::endl;
+	for (int i = 0; i < vec.size(); i++) {
+		std::cout << " " << vec[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+////////////////////////
+///////// SWAP /////////
+////////////////////////
+template<typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+////////////////////////
+////////////////////////
+//// INSERTION SORT ////
+////////////////////////
+////////////////////////
+
 template<typename T>
 void insertionSort(std::vector<T>& vec) {
 
@@ -17,13 +43,17 @@ void insertionSort(std::vector<T>& vec) {
 
 }
 
-
+/////////////////////////
+/////////////////////////
+////// MERGE SORT ///////
+/////////////////////////
+/////////////////////////
 
 template<typename T>
 void merge(std::vector<T>& vec, int beg, int mid, int end) {
 
 	int i = beg, j = mid + 1;
-	std::vector<T> temp(end - beg + 1);
+	std::vector<T> temp(end - beg + 1,0);
 	int k = 0;
 
 	while (i <= mid && j <= end) {
@@ -38,12 +68,14 @@ void merge(std::vector<T>& vec, int beg, int mid, int end) {
 		k++;
 	}
 
-	while (i++ <= mid) {
+	while (i <= mid) {
 		temp[k++] = vec[i];
+		i++;
 	}
 
-	while (j++ <= end) {
+	while (j <= end) {
 		temp[k++] = vec[j];
+		j++;
 	}
 
 	for (k = 0; beg + k <= end; k++) {
@@ -71,5 +103,94 @@ void mergeSort(std::vector<T>& vec) {
 
 
 
+
+///////////////////////////
+///////////////////////////
+/////// BUBBLE SORT ///////
+///////////////////////////
+///////////////////////////
+
+template<typename T>
+void bubbleSort(std::vector<T>& vec) {
+	int lastUnsorted = vec.size()-1;
+	bool swapped;
+	do {
+		swapped = false;
+		for (int i = 0; i < lastUnsorted; i++) {
+			if (vec[i] > vec[i + 1]) {
+				swap(vec[i], vec[i + 1]);
+				/*vec[i] = vec[i] + vec[i + 1];
+				vec[i + 1] = vec[i] - vec[i + 1];
+				vec[i] = vec[i] - vec[i + 1];*/
+				swapped = true;
+			}
+		}
+		lastUnsorted--;
+		
+	} while (swapped);
+}
+
+
+
+
+//////////////////////////////////
+//////////////////////////////////
+////////// QUICK SORT ////////////
+//////////////////////////////////
+//////////////////////////////////
+
+template<typename T>
+int partition(std::vector<T>& vec, int beg, int end) {
+
+	int pIndex = beg;
+	T pivotElem = vec[beg];
+
+	for (int i = beg + 1; i < end + 1; i++) {
+		if (vec[i] <= pivotElem) {
+
+			pIndex++;
+			swap(vec[pIndex], vec[i]);
+		
+		}
+	}
+
+	swap(vec[pIndex], vec[beg]);
+
+	return pIndex;
+}
+
+template<typename T>
+void qSort(std::vector<T>& vec, int beg, int end) {
+	if (beg < end) {
+		int p = partition(vec, beg, end);
+		qSort(vec, beg, p - 1);
+		qSort(vec, p + 1, end);
+	}
+}
+
+template<typename T>
+void quickSort(std::vector<T>& vec) {
+	qSort(vec, 0, vec.size() - 1);
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+///////// SELECTION SORT ///////////
+////////////////////////////////////
+////////////////////////////////////
+template<typename T>
+void selectionSort(std::vector<T>& vec) {
+	int minIndex;
+	for (int i = 0; i < vec.size(); i++) {
+		minIndex = i;
+		for (int j = i + 1; j < vec.size(); j++) {
+			if (vec[j] < vec[minIndex])
+				minIndex = j;
+		}
+		swap(vec[i], vec[minIndex]);
+	}
+
+}
 
 
